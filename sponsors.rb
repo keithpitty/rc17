68971c4a38878a72c45f71b3e8d6f069e71ad445
Keith Pitty <keith@keithpitty.com>
2015-06-11 20:58:44 +1000

    Sponsors

---
+++ sponsors.rb
@@ -0,0 +1,14 @@
+class Sponsors
+  WHAT_BY_WHOM = {
+    culture_amp: :train_tickets,
+    rea: :drinks,
+    reinteractive: :student_tickets,
+    fat_zebra: :student_tickets,
+    buildkite: :gender_pay_gap_tickets,
+    siteminder: :shirts,
+    lookahead: :coffee_machines,
+    blake: :coffee_beans,
+    sample: :coffee_especially_filtered,
+    redbubble: :opening_bbq
+  }
+end
